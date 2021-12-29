cf437b8959ee11e2c287ebe72bb8257c2de210ec
americanium <96286886+americanium@users.noreply.github.com>
2021-12-29 22:50:07 +0100

    Update SmartTest.py

--- SmartTest.py
+++ SmartTest.py
@@ -1,224 +1,102 @@
 #!/usr/bin/env python
 """
-Pymodbus Asynchronous Server Example
+Pymodbus Server With Updating Thread
 --------------------------------------------------------------------------
-The asynchronous server is a high performance implementation using the
-twisted library as its backend.  This allows it to scale to many thousands
-of nodes which can be helpful for testing monitoring software.
+
+This is an example of having a background thread updating the
+context while the server is operating. This can also be done with
+a python thread::
+
+    from threading import Thread
+
+    thread = Thread(target=updating_writer, args=(context,))
+    thread.start()
 """
-# --------------------------------------------------------------------------- # 
-# import the various server implementations
 # --------------------------------------------------------------------------- #
-import sys
-import paho.mqtt.client as mqtt
-sys.path.append('/usr/local/lib//python3.7/dist-packages')
-
+# import the modbus libraries we need
+# --------------------------------------------------------------------------- #
 from pymodbus.version import version
-from pymodbus.server.asynchronous import StartTcpServer
-from pymodbus.server.asynchronous import StartUdpServer
 from pymodbus.server.asynchronous import StartSerialServer
-
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
-from pymodbus.datastore import ModbusSparseDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-from pymodbus.transaction import (ModbusRtuFramer,
-                                  ModbusAsciiFramer,
-                                  ModbusBinaryFramer)
-from custom_message import CustomModbusRequest
+from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
 
+# --------------------------------------------------------------------------- #
+# import the twisted libraries we need
+# --------------------------------------------------------------------------- #
+from twisted.internet.task import LoopingCall
 
-# --------------------------------------------------------------------------- # 
+# --------------------------------------------------------------------------- #
 # configure the service logging
-# --------------------------------------------------------------------------- # 
+# --------------------------------------------------------------------------- #
 import logging
-import thread
-FORMAT = ('%(asctime)-15s %(threadName)-15s'
-          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
-logging.basicConfig(format=FORMAT)
+logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-def run_async_server():
-
-    # ----------------------------------------------------------------------- # 
-    # initialize your data store
-    # ----------------------------------------------------------------------- # 
-    # The datastores only respond to the addresses that they are initialized to
-    # Therefore, if you initialize a DataBlock to addresses from 0x00 to 0xFF,
-    # a request to 0x100 will respond with an invalid address exception.
-    # This is because many devices exhibit this kind of behavior (but not all)
-    #
-    # block = ModbusSequentialDataBlock(0x05, [11]*1)
-    #
-    # Continuing, you can choose to use a sequential or a sparse DataBlock in
-    # your data context.  The difference is that the sequential has no gaps in
-    # the data while the sparse can. Once again, there are devices that exhibit
-    # both forms of behavior::
-    #
-    #     block = ModbusSparseDataBlock({0x00: 0, 0x05: 1})
-    #     block = ModbusSequentialDataBlock(0x00, [0]*5)
-    #
-    # Alternately, you can use the factory methods to initialize the DataBlocks
-    # or simply do not pass them to have them initialized to 0x00 on the full
-    # address range::
-    #
-    #     store = ModbusSlaveContext(di = ModbusSequentialDataBlock.create())
-    #     store = ModbusSlaveContext()
-    #
-    # Finally, you are allowed to use the same DataBlock reference for every
-    # table or you you may use a seperate DataBlock for each table.
-    # This depends if you would like functions to be able to access and modify
-    # the same data or not::
-    #
-    #     block = ModbusSequentialDataBlock(0x00, [0]*0xff)
-    #     store = ModbusSlaveContext(di=block, co=block, hr=block, ir=block)
-    #
-    # The server then makes use of a server context that allows the server to
-    # respond with different slave contexts for different unit ids. By default
-    # it will return the same context for every unit id supplied (broadcast
-    # mode).
-    # However, this can be overloaded by setting the single flag to False
-    # and then supplying a dictionary of unit id to context mapping::
-    #
-    slaves  = {
-    0x01: ModbusSlaveContext(),
-    #         0x02: ModbusSlaveContext(...),
-    #         0x03: ModbusSlaveContext(...),
-    }
-    context = ModbusServerContext(slaves=slaves, single=False)
-    #
-    # The slave context can also be initialized in zero_mode which means that a
-    # request to address(0-7) will map to the address (0-7). The default is
-    # False which is based on section 4.4 of the specification, so address(0-7)
-    # will map to (1-8)::
-    #
-    #     store = ModbusSlaveContext(..., zero_mode=True)
-    # ----------------------------------------------------------------------- # 
-    store = ModbusSlaveContext(
-        di=ModbusSequentialDataBlock(0, [17]*100),
-        co=ModbusSequentialDataBlock(0, [17]*100),
-        hr=ModbusSparseDataBlock({
-
-	40001: [21365, 28243],
-	40003: [1, 65],
-40005: [0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0],
-
-40070: [213, 124],
-
-	40072: [0,0,0,0,0,0,0,0,0,0,
-		0,0,0,0,0,0,0,0,0,0,
-		0,0,0,0,0,0,17530,
-0,0,0,0,0,0,0,0,0,0	,
-0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0
-],
-
-40196: [65535, 0]
- }),
-
-
-ir=ModbusSequentialDataBlock(0, [17]*100))
-    store.register(CustomModbusRequest.function_code, 'cm',
-                   ModbusSequentialDataBlock(5, [11] * 1))
-    context = ModbusServerContext(slaves=store, single=True)
-
-    # ----------------------------------------------------------------------- # 
-    # initialize the server information
-    # ----------------------------------------------------------------------- # 
-    # If you don't set this or any fields, they are defaulted to empty strings.
-    # ----------------------------------------------------------------------- # 
-    identity = ModbusDeviceIdentification()
-    identity.VendorName = 'Pymodbus'
-    identity.ProductCode = 'PM'
-    identity.VendorUrl = 'http://github.com/riptideio/pymodbus/'
-    identity.ProductName = 'Pymodbus Server'
-    identity.ModelName = 'Pymodbus Server'
-    identity.MajorMinorRevision = version.short()
-    
-    # ----------------------------------------------------------------------- # 
-    # run the server you want
-    # ----------------------------------------------------------------------- # 
-
-    # TCP Server
-
-    #StartTcpServer(context, identity=identity, address=("localhost", 5020),
-    #               custom_functions=[CustomModbusRequest])
-
-    # TCP Server with deferred reactor run
-
-    # from twisted.internet import reactor
-    # StartTcpServer(context, identity=identity, address=("localhost", 5020),
-    #                defer_reactor_run=True)
-    # reactor.run()
-
-    # Server with RTU framer
-    # StartTcpServer(context, identity=identity, address=("localhost", 5020),
-    #                framer=ModbusRtuFramer)
-
-    # UDP Server
-    # StartUdpServer(context, identity=identity, address=("127.0.0.1", 5020))
-
-    # RTU Server
-    StartSerialServer(context, identity=identity,
-                      port='/dev/ttyUSB1', baudrate=9600, stopbits=1, bytesize=8, framer=ModbusRtuFramer)
-
-    # ASCII Server
-    # StartSerialServer(context, identity=identity,
-    #                   port='/dev/ttyp0', framer=ModbusAsciiFramer)
-
-    # Binary Server
-    # StartSerialServer(context, identity=identity,
-    #                   port='/dev/ttyp0', framer=ModbusBinaryFramer)
-
-
-
-
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("AMIS/#")
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    #print(msg.topic+" "+str(msg.payload))
-    testvariable = str(msg.payload)
-    print(testvariable)
-    return testvariable
-    #print(testvariable)
+# --------------------------------------------------------------------------- #
+# configure MQTT service
+# --------------------------------------------------------------------------- #
+import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
+
+client.connect("localhost", 1883, 60)
+client.username_pw_set("xxx", password="xxx")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("AMIS/#")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
 client.on_connect = on_connect
 client.on_message = on_message
 
-#client.connect("localhost", 1883, 60)
-client.username_pw_set("loxberry", password="nsQMdsC1Ok47v6Ok")
-client.connect("localhost", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+# --------------------------------------------------------------------------- #
+# define your callback process
+# --------------------------------------------------------------------------- #
 
 
+def updating_writer(a):
+    """ A worker process that runs every so often and
+    updates live values of the context. It should be noted
+    that there is a race condition for the update.
 
+    :param arguments: The input arguments to the call
+    """
+    log.debug("updating the context")
+    context = a[0]
+    register = 3
+    slave_id = 0x01
+    address = 0x9C40
+    values = [1200, 1201]
+    log.debug("new values: " + str(values))
+    context[slave_id].setValues(register, address, values)
+
+def run_updating_server():
+    # ----------------------------------------------------------------------- # 
+    # initialize your data store
+    # ----------------------------------------------------------------------- # 
+    
+    store = ModbusSlaveContext(
+        di=ModbusSequentialDataBlock(0, [15]*100),
+        co=ModbusSequentialDataBlock(0, [15]*100),
+        hr=ModbusSequentialDataBlock(40001, [15]*2),
+        ir=ModbusSequentialDataBlock(0, [15]*100))
+    context = ModbusServerContext(slaves=store, single=True)
+
+    # ----------------------------------------------------------------------- # 
+    # run the server you want
+    # ----------------------------------------------------------------------- # 
+    time = 5  # 5 seconds delay
+    loop = LoopingCall(f=updating_writer, a=(context,))
+    loop.start(time, now=False) # initially delay by time
+    #StartTcpServer(context, identity=identity, address=("localhost", 5020))
+    StartSerialServer(context, port='/dev/ttyUSB1', baudrate=9600, stopbits=1, bytesize=8, framer=ModbusRtuFramer)
 
 if __name__ == "__main__":
-    run_async_server()
+    run_updating_server()
