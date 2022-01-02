48df57c2a92b080dd50b24f5a7764b0e924d1954
americanium <96286886+americanium@users.noreply.github.com>
2022-01-02 21:10:08 +0100

    Update SmartTest.py

--- SmartTest.py
+++ SmartTest.py
@@ -65,90 +65,99 @@ def updating_writer(a):
     :param arguments: The input arguments to the call
     """
     msg = subscribe.simple("AMIS/Leistung", hostname="localhost",
-    port=1883, auth={'username':"XXXXXXXXXX",'password':"XXXXXXXXXXXXX"})
-    print("%s %s" % (msg.topic, msg.payload))
+    port=1883, auth={'username':"loxberry",'password':"nsQMdsC1Ok47v6Ok"})
+    print(msg.topic+" "+str(msg.payload))
+
+    msg1 = subscribe.simple("AMIS/Netzbezug_total", hostname="localhost",
+    port=1883, auth={'username':"loxberry",'password':"nsQMdsC1Ok47v6Ok"})
+    print("%s %s" % (msg1.topic, msg1.payload))
+
+    msg2 = subscribe.simple("AMIS/Netzeinspeisung_total", hostname="localhost",
+    port=1883, auth={'username':"loxberry",'password':"nsQMdsC1Ok47v6Ok"})
+    print("%s %s" % (msg2.topic, msg2.payload))
+
+    #Converting current power consumption out of MQTT payload to Modbus register
 
     electrical_power_float = float(msg.payload) #extract value out of payload
-    electrical_power_hex = struct.pack('>f', electrical_power_float).encode('hex') #convert float value to float32 and furhter to hex
-    print electrical_power_hex
-    hex_part1 = str(electrical_power_hex)[0:4]
-    hex_part2 = str(electrical_power_hex)[4:8]
-    print hex_part1
-    print hex_part2
-    dec_1 = int(hex_part1, 16)
-    dec_2 = int(hex_part2, 16)
-    print dec_1
-    print dec_2
+    print electrical_power_float
+    electrical_power_hex = struct.pack('>f', electrical_power_float).encode('hex') #convert float value to float32 and further to hex
+    electrical_power_hex_part1 = str(electrical_power_hex)[0:4] #extract first register part (hex)
+    electrical_power_hex_part2 = str(electrical_power_hex)[4:8] #extract seconds register part (hex)
+    ep_int1 = int(electrical_power_hex_part1, 16) #convert hex to integer because pymodbus converts back to hex itself
+    ep_int2 = int(electrical_power_hex_part2, 16) #convert hex to integer because pymodbus converts back to hex itself
+
+    #Converting total import value of smart meter out of MQTT payload into Modbus register
+
+    total_import_float = int(msg1.payload)
+    print total_import_float
+    total_import_hex = struct.pack('>f', total_import_float).encode("hex")
+    total_import_hex_part1 = str(total_import_hex)[0:4]
+    total_import_hex_part2 = str(total_import_hex)[4:8]
+    ti_int1  = int(total_import_hex_part1, 16)
+    ti_int2  = int(total_import_hex_part2, 16)
+
+    #Converting total export value of smart meter out of MQTT payload into Modbus register
+
+    total_export_float = int(msg2.payload)
+    print total_export_float
+    total_export_hex = struct.pack('>f', total_export_float).encode("hex")
+    total_export_hex_part1 = str(total_export_hex)[0:4]
+    total_export_hex_part2 = str(total_export_hex)[4:8]
+    exp_int1 = int(total_export_hex_part1, 16)
+    exp_int2 = int(total_export_hex_part2, 16)
 
     log.debug("updating the context")
     context = a[0]
     register = 3
     slave_id = 0x01
     address = 0x9C87
-    values = [16448, 0, #Ampere - AC Total Current Value [A]
-              16448, 0, #Ampere - AC Current Value L1 [A]
-              16448, 0, #Ampere - AC Current Value L2 [A]
-              16448, 0, #Ampere - AC Current Value L3 [A]
-              17254, 0, #Voltage - Average Phase to Neutral [V]
-              17254, 0, #Voltage - Phase L1 to Neutral [V]
-              17254, 0, #Voltage - Phase L2 to Neutral [V]
-              17254, 0, #Voltage - Phase L3 to Neutral [V]
-              17254, 0, #Voltage - Average Phase to Phase [V]
-              17254, 0, #Voltage - Phase L1 to L2 [V]
-              17254, 0, #Voltage - Phase L2 to L3 [V]
-              17254, 0, #Voltage - Phase L1 to L3 [V]
-              16968, 0, #AC Frequency [Hz]
-              dec_1, 0, #AC Power value (Total) [W]
-              17402, 0, #AC Power Value L1 [W]
-              17402, 0, #AC Power Value L2 [W]
-              17402, 0, #AC Power Value L3 [W]
-              0,0,      #AC Apparent Power [VA]
-              0,0,      #AC Apparent Power L1 [VA]
-              0,0,      #AC Apparent Power L2 [VA]
-              0,0,      #AC Apparent Power L3 [VA]
-              0,0,      #AC Reactive Power [VAr]
-              0,0,      #AC Reactive Power L1 [VAr]
-              0,0,      #AC Reactive Power L2 [VAr]
-              0,0,      #AC Reactive Power L3 [VAr]
-              0,0,
-              0,0,
-              0,0, 
-              0,0, 
-              0,0,
-              0,0,
-              0,0,
-              0,0,
-              0,0, 
-              0,0,
-              0,0,
-              0,0,
-              6,0,
-              0,0,
-              0,0,
-              0,0,
-              0,0,
-              0,0,
-              0,0,
-              0,0,
-              0,0,
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0,   #NaN (Not supported) 32704 = 7FC0 = NaN
-              32704,0    #NaN (Not supported) 32704 = 7FC0 = NaN
+    values = [0, 0,               #Ampere - AC Total Current Value [A]
+              0, 0,               #Ampere - AC Current Value L1 [A]
+              0, 0,               #Ampere - AC Current Value L2 [A]
+              0, 0,               #Ampere - AC Current Value L3 [A]
+              0, 0,               #Voltage - Average Phase to Neutral [V]
+              0, 0,               #Voltage - Phase L1 to Neutral [V]
+              0, 0,               #Voltage - Phase L2 to Neutral [V]
+              0, 0,               #Voltage - Phase L3 to Neutral [V]
+              0, 0,               #Voltage - Average Phase to Phase [V]
+              0, 0,               #Voltage - Phase L1 to L2 [V]
+              0, 0,               #Voltage - Phase L2 to L3 [V]
+              0, 0,               #Voltage - Phase L1 to L3 [V]
+              0, 0,               #AC Frequency [Hz]
+              ep_int1, 0,         #AC Power value (Total) [W] ==> Second hex word not needed
+              0, 0,               #AC Power Value L1 [W]
+              0, 0,               #AC Power Value L2 [W]
+              0, 0,               #AC Power Value L3 [W]
+              0, 0,               #AC Apparent Power [VA]
+              0, 0,               #AC Apparent Power L1 [VA]
+              0, 0,               #AC Apparent Power L2 [VA]
+              0, 0,               #AC Apparent Power L3 [VA]
+              0, 0,               #AC Reactive Power [VAr]
+              0, 0,               #AC Reactive Power L1 [VAr]
+              0, 0,               #AC Reactive Power L2 [VAr]
+              0, 0,               #AC Reactive Power L3 [VAr]
+              0, 0,	          #AC power factor total [cosphi]
+              0, 0,               #AC power factor L1 [cosphi]
+              0, 0,               #AC power factor L2 [cosphi]
+              0, 0,               #AC power factor L3 [cosphi]
+              exp_int1, exp_int2, #Total Watt Hours Exportet [Wh] ==> 11968Wh == JSON: EnergyReal_WAC_Minus_Absolute
+              0, 0,               #Watt Hours Exported L1 [Wh] ==> 4000 ==> JSON: EnergyReal_WAC_Phase_1_Produced
+              0, 0,               #Watt Hours Exported L2 [Wh] ==> 4000 ==> JSON: EnergyReal_WAC_Phase_2_Produced
+              0, 0,               #Watt Hours Exported L3 [Wh] ==> 4000 ==> JSON: EnergyReal_WAC_Phase_3_Produced
+              ti_int1, ti_int2,   #Total Watt Hours Imported [Wh]  1500Wh JSON: EnergyReal_WAC_Plus_Absolute
+              0, 0,               #Watt Hours Imported L1 [Wh] 10000 ==> JSON: EnergyReal_WAC_Phase_1_Consumed
+              0, 0,               #Watt Hours Imported L2 [Wh] 5000 ==> JSON: EnergyReal_WAC_Phase_2_Consumed
+              0, 0,               #Watt Hours Imported L3 [Wh] 5000 ==> JSON: EnergyReal_WAC_Phase_3_Consumed
+              0, 0,               #Total VA hours Exported [VA]
+              0, 0,               #VA hours Exported L1 [VA]
+              0, 0,               #VA hours Exported L2 [VA]
+              0, 0,               #VA hours Exported L3 [VA]
+              0, 0,               #Total VAr hours imported [VAr]
+              0, 0,               #VA hours imported L1 [VAr]
+              0, 0,               #VA hours imported L2 [VAr]
+              0, 0                #VA hours imported L3 [VAr]
 ]
-    log.debug("new values: " + str(dec_1) + str(dec_2))
+    #log.debug("new values: " + str(dec_1) + str(dec_2))
     context[slave_id].setValues(register, address, values)
 
 def run_updating_server():
@@ -161,16 +170,13 @@ def run_updating_server():
         co=ModbusSequentialDataBlock(0, [15]*100),
         hr=ModbusSparseDataBlock({
         
-        #0:      [0],
-        00001:   [0],
-        00002:   [2300],
-        00012:   [240],
-        #769:    [0],
-        #1707:   [0],
+        #00001:   [0],
+        #00002:   [2300],
+        #00012:   [240],
         40001:  [21365, 28243], 
         40003:  [1],
         40004:  [65],
-        40005:  [70,114,111,110,105,117,0,0,0,0,
+        40005:  [70,114,111,110,105,117,115,0,0,0,
                 0,0,0,0,0,0,83,109,97,114,
                 116,32,77,101,116,101,114,32,54,51,
                 65,0,0,0,0,0,0,0,0,0,
@@ -178,15 +184,14 @@ def run_updating_server():
                 50,50,48,49,49,56,0,0,0,0,
                 0,0,0,0,
 
-                0,240], 
-        
-        #40010: [0,0,0,0,0,0,0,0,0,0],
-
+                0,240],
+        40069: [240], 
+        40010: [0,0,0,0,0,0,0,0,0,0],
         40070: [213],
         40071: [124], 
         40072: [0,0,0,0,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
-                0,0,0,0,0,0,17530,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
@@ -197,8 +202,8 @@ def run_updating_server():
                 0,0,0,0,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
                 0,0,0,0],
+
                 40196: [65535, 0],
-         #      50001: [0,0]
 
 }),
 
